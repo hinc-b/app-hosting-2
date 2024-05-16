1fa5ec233fee9078055b510648f926352bd46ecc
Blazej Hinc <blazejh@google.com>
2024-05-16 15:30:28 +0200

    useUserSession change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,43 +11,27 @@ import { useRouter } from "next/navigation";
 import { firebaseConfig } from "@/src/lib/firebase/config";
 
 function useUserSession(initialUser) {
-	// The initialUser comes from the server via a server component
+	// The initialUser comes from the server through a server component
 	const [user, setUser] = useState(initialUser);
 	const router = useRouter();
 
-	// Register the service worker that sends auth state back to server
-	// The service worker is built with npm run build-service-worker
 	useEffect(() => {
-			if ("serviceWorker" in navigator) {
-					const serializedFirebaseConfig = encodeURIComponent(JSON.stringify(firebaseConfig));
-					const serviceWorkerUrl = `/auth-service-worker.js?firebaseConfig=${serializedFirebaseConfig}`
-			
-			  navigator.serviceWorker
-					.register(serviceWorkerUrl)
-					.then((registration) => console.log("scope is: ", registration.scope));
-			}
-	  }, []);
-
-	useEffect(() => {
-			const unsubscribe = onAuthStateChanged((authUser) => {
-					setUser(authUser)
-			})
-
-			return () => unsubscribe()
-			// eslint-disable-next-line react-hooks/exhaustive-deps
+			const unsubscribe = onAuthStateChanged(authUser => {
+					setUser(authUser);
+			});
+			return () => {
+					unsubscribe();
+			};
 	}, []);
 
 	useEffect(() => {
-			onAuthStateChanged((authUser) => {
-					if (user === undefined) return
-
-					// refresh when user changed to ease testing
+			onAuthStateChanged(authUser => {
+					if (user === undefined) return;
 					if (user?.email !== authUser?.email) {
-							router.refresh()
+							router.refresh();
 					}
-			})
-			// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [user])
+			});
+	}, [user]);
 
 	return user;
 }
